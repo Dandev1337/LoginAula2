68b9bd98db2697cb293420a0654c3e17d9941357
Daniel Oliveira <devdan1337@gmail.com>
2023-10-16 15:59:53 -0300

    atividades 16/10

--- app/src/main/java/com/danieloliveira/login/MainActivity.java
+++ app/src/main/java/com/danieloliveira/login/MainActivity.java
@@ -2,13 +2,34 @@ package com.danieloliveira.login;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
-
+    EditText edtEmail, edtsenha;
+    Button btnEntra;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.login);
+
+        edtEmail = findViewById(R.id.edtEmail);
+        edtsenha = findViewById(R.id.edtSenha);
+        btnEntra = findViewById(R.id.btnEntrar);
+
+        btnEntra.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent redirecionar = new Intent(MainActivity.this, MainActivity2.class);
+                startActivity(it);
+
+            }
+        });
+
     }
+
 }
